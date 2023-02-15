12db785b2a7c92cf0bba1c6fb93f4e8000944f3d
akamsali <akamsali@purdue.edu>
2023-02-15 17:18:18 -0500

    HW4 all plots done

--- HW4/models.py
+++ HW4/models.py
@@ -9,7 +9,7 @@ class HW4Net1(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)  
         self.conv2 = nn.Conv2d(16, 32, 3) 
         self.fc1 = nn.Linear(32*14*14 , 64)
-        self.fc2 = nn.Linear(64, 10)
+        self.fc2 = nn.Linear(64, 5)
 
     def forward(self , x):
         # b x 3 x 64 x 64 -> b x 16 x 62 x 62 -> b x 16 x 31 x 31
@@ -30,7 +30,7 @@ class HW4Net2(nn.Module):
         self.pool = nn.MaxPool2d(2, 2) 
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.fc1 = nn.Linear(32*16*16 , 64) 
-        self.fc2 = nn.Linear(64, 10)
+        self.fc2 = nn.Linear(64, 5)
 
     def forward(self , x):
         # b x 3 x 64 x 64 -> b x 16 x 64 x 64 -> b x 16 x 32 x 32
@@ -43,21 +43,29 @@ class HW4Net2(nn.Module):
         return x
 
 
-# Net 2
-class HW4Net2(nn.Module):
-    def __init__(self):
-        super(HW4Net2, self).__init__ ()
+# Net 3
+class HW4Net3(nn.Module):
+    def __init__(self, num_chained=10):
+        super(HW4Net3, self).__init__ ()
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1) 
         self.pool = nn.MaxPool2d(2, 2) 
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        
+        self.conv_chained = nn.ModuleList([nn.Conv2d(32, 32, 3, padding=1) for _ in range(num_chained)])
+        # since
         self.fc1 = nn.Linear(32*16*16 , 64) 
-        self.fc2 = nn.Linear(64, 10)
+        self.fc2 = nn.Linear(64, 5)
 
     def forward(self , x):
         # b x 3 x 64 x 64 -> b x 16 x 64 x 64 -> b x 16 x 32 x 32
         x = self.pool(F.relu(self.conv1(x)))
         # b x 16 x 32 x 32 -> b x 32 x 32 x 32 -> b x 32 x 16 x 16
         x = self.pool(F.relu(self.conv2(x)))
+
+        # since no pooling same output b x 32 x 16 x 16 -> b x 32 x 16 x 16
+        for cl in self.conv_chained:
+            x = cl(x)
+
         x = x.view(x.shape[0], -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
