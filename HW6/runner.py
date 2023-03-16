006c4e9de50b22a7161d466982eac9c6354b9174
jo <wang3450@purdue.edu>
2023-03-16 01:21:59 +0000

    modify trainer

--- HW6/runner.py
+++ HW6/runner.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 categories = ["bus", "cat", "pizza"]
 
 batch_size = 4
-train_data = MyDataset(categories=categories, split='train', manifest_path='./manifests', mac=True)
+train_data = MyDataset(categories=categories, split='train', manifest_path='./manifests')
 
 train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
@@ -25,24 +25,24 @@ criterion1 = nn.BCELoss()                    # For the first element of the 8 el
 criterion2 = nn.MSELoss()                    # For the regression elements (indexed 2,3,4,5) of yolo vector   ## (4)
 criterion3 = nn.CrossEntropyLoss()           # For the last three elements of the 8 element yolo vector        ## (5)
 sigmoid = nn.Sigmoid()
-softmax = nn.functional.softmax
+
 # print("\n\nLearning Rate: ", self.rpg.learning_rate)
 optimizer = optim.Adam(ynet.parameters(), lr=1e-4)                 ## (6)
 epochs = 2
 # total_loss = []
-logger = open('./solutions/test.csv', 'a')
+logger = open('./solutions/test.csv', 'a', newline="")
 loss_flag = 1e32
 ynet = ynet.to(device)
 for epoch in range(epochs):
     running_loss = 0
     for i, d in tqdm(enumerate(train_loader)):
-        inp, _, true_yolo_aug = d
+        inp, t_l, true_yolo_aug = d
         optimizer.zero_grad()
         inp = inp.to(device)
         true_yolo_aug = true_yolo_aug.to(device)
 
         out = ynet(inp)
-        out = out.view(batch_size, 64, 5, -1)
+        out = out.view(-1, 64, 5, 9)
         # print(out.shape)
         # pred_objectness = out[:, :, :, 0]
         # regression_box = out[:, :, :, 1:5]
@@ -54,32 +54,33 @@ for epoch in range(epochs):
         present_obj = torch.nonzero(true_yolo_aug[:, :, :, 0])#.unsqueeze(0)
 
         # print(present_obj)
-        pred_objectness = torch.zeros((len(present_obj)))
-        true_objectness = torch.ones((len(present_obj)))
+        #pred_objectness = torch.zeros((len(present_obj)))
+        #true_objectness = torch.ones((len(present_obj)))
 
 
         pred_regression_box = torch.zeros((len(present_obj), 4))
         true_regression_box = torch.zeros((len(present_obj), 4))
 
-        pred_labels = torch.zeros((len(present_obj)))
-        true_labels = torch.zeros((len(present_obj)))
+        pred_labels = torch.zeros((len(present_obj), 3))
+        true_labels = torch.zeros((len(present_obj), 3))
 
-        for i, p in enumerate(present_obj):
+        for j, p in enumerate(present_obj):
             # pred_objectness[i] = out[p[0], p[1], p[2], 0]
             # print(p)
-            pred_regression_box[i] = out[p[0], p[1], p[2], 1:5]
-            true_regression_box[i] = true_yolo_aug[p[0], p[1], p[2], 1:5]
+            pred_regression_box[j] = out[p[0], p[1], p[2], 1:5]
+            true_regression_box[j] = true_yolo_aug[p[0], p[1], p[2], 1:5]
 
-            pred_labels[i] = torch.argmax(softmax(out[p[0], p[1], p[2], 5:-1], dim=0))
-            true_labels[i] = torch.argmax(true_yolo_aug[p[0], p[1], p[2], 5:-1])
-        
-        loss_BCE = criterion1(sigmoid(pred_objectness.unsqueeze(0)), true_objectness.unsqueeze(0))
+            pred_labels[j] = out[p[0], p[1], p[2], 5:-1]
+            true_labels[j] = true_yolo_aug[p[0], p[1], p[2], 5:-1]
+        #print(pred_labels, true_labels, t_l)
+        #break
+        loss_BCE = criterion1(sigmoid(out[:, :, : , 0]), true_yolo_aug[:, :, :, 0])
         # print(loss_BCE)
 
-        loss_MSE = criterion2(pred_regression_box.unsqueeze(0), true_regression_box.unsqueeze(0))
+        loss_MSE = criterion2(pred_regression_box, true_regression_box)
         # print(loss_MSE)
 
-        loss_CE = criterion3(pred_labels.unsqueeze(0), true_labels.unsqueeze(0))
+        loss_CE = criterion3(pred_labels, true_labels)
         # print(loss_CE)
 
         total_loss = loss_BCE + loss_MSE + loss_CE
@@ -89,7 +90,9 @@ for epoch in range(epochs):
         running_loss += total_loss.item()
 
         if i % 100 == 0:
-            data = [epoch + 1, i + 1, loss_BCE.item(), loss_MSE(), loss_CE.item(), running_loss / 100]
+ 
+            logger = open('./solutions/test.csv', 'a', newline="")           
+            data = [epoch + 1, i + 1, loss_BCE.item(), loss_MSE.item(), loss_CE.item(), running_loss / 100]
             with logger:
                 write = csv.writer(logger)
                 write.writerow(data)
