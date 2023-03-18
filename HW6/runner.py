bea8d98ce794d7ee7c5d00b5118ae0edba49bd99
Akshita Kamsali <akamsali@purdue.edu>
2023-03-17 20:34:48 -0700

    modify script

--- HW6/runner.py
+++ HW6/runner.py
@@ -8,9 +8,9 @@ from torch.utils.data import DataLoader
 categories = ["bus", "cat", "pizza"]
 
 batch_size = 4
-train_data = MyDataset(
-    categories=categories, split="train", manifest_path="./manifests", mac=True
-)
+# train_data = MyDataset(
+#     categories=categories, split="train", manifest_path="./manifests", mac=True
+# )
 train_data = MyDataset(
     categories=categories, split="train", manifest_path="./manifests"
 )
@@ -19,9 +19,10 @@ train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 ynet = NetForYolo(depth=2)
 
 train(train_loader, ynet)
-val_data = MyDataset(
-    categories=categories, split="val", manifest_path="./manifests", mac=True
-)
-val_data = MyDataset(categories=categories, split="val", manifest_path="./manifests")
-val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
-validation(val_loader, ynet)
+
+# val_data = MyDataset(
+#     categories=categories, split="val", manifest_path="./manifests", mac=True
+# )
+# val_data = MyDataset(categories=categories, split="val", manifest_path="./manifests")
+# val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+# validation(val_loader, ynet)
