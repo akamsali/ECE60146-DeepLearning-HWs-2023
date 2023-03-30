9c3146b0c29a1d649e1e1b7a9874df93c5a5a094
jo <wang3450@purdue.edu>
2023-03-30 14:59:31 +0000

    train gan

--- HW7/runner.py
+++ HW7/runner.py
@@ -9,4 +9,4 @@ train_dataset = MyDataset()
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-train_gan(train_loader, epochs=1, name="gan", device=device)
+train_gan(train_loader, epochs=10, name="gan", device=device)
